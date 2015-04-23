be708c530639fc0e7bc31935924fccefb4f40fcd
huliang7777 <364242517@qq.com>
2015-04-23 22:09:52 +0800

    创建项目

---
+++ src/com/iguyue/canvas/WaveBallActivity.java
@@ -0,0 +1,13 @@
+package com.iguyue.canvas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WaveBallActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_wave_ball);
+	}
+}
