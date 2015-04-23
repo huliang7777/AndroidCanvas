095f34ca6424b6b50055c3af175ece4d68e8e828
huliang7777 <364242517@qq.com>
2015-04-23 22:57:27 +0800

    尝试第一次提交

---
+++ src/com/iguyue/canvas/widget/WaveBall.java
@@ -0,0 +1,27 @@
+package com.iguyue.canvas.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class WaveBall extends View 
+{
+	public WaveBall(Context context) 
+	{
+		super(context);
+	}
+
+	public WaveBall(Context context, AttributeSet attrs) 
+	{
+		super(context, attrs);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) 
+	{
+		super.onDraw(canvas);
+	}
+	
+
+}
